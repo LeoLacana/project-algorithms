07d792ab5c189901e98f90d71296a4e3f29b5305
LeoLacana <wilson17414@gmail.com>
2022-02-21 22:20:09 -0300

    fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,10 @@
-from cmath import log
-
-
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     counter = 0
     try:
         for period in permanence_period:
             if (period[0] <= target_time <= period[1]):
-                    counter += 1
+                counter += 1
     except TypeError:
         return None
     return counter
