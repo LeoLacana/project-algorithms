bf3a3bc14b1e7650f805d215c6660c694b509094
LeoLacana <wilson17414@gmail.com>
2022-02-24 20:26:04 -0300

    fix lint"

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,10 +1,7 @@
-from pyparsing import Word
-
-
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if (word == '' or word[low_index] != word[high_index]):
         return False
     if (len(word) - 1)//2 == low_index:
         return True
-    return is_palindrome_recursive(word, low_index + 1, high_index -1)
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
