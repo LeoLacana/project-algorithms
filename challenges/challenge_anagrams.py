291d901ef9e76afa1f1abcdae2d43319f85409d5
LeoLacana <wilson17414@gmail.com>
2022-02-25 03:35:11 -0300

    two parts of requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,4 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if (first_string != "" or second_string != ""):
+        return False
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,8 +2,8 @@ def find_duplicate(nums):
     """ Faça o código aqui. """
     if not (nums):
         return False
+    if (len(nums) < 2):
+        return False
     for number in nums:
         if (type(number) == str or number < 0):
             return False
-    if (len(nums) < 2):
-        return False
